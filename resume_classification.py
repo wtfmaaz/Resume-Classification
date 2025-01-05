fb0e161f12f918b3755e03aedcadfb033f91d985
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2025-01-05 15:29:54 +0530

    Update resume_classification.py

--- resume_classification.py
+++ resume_classification.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/12n_0fxjvIRrPJtGgLL5RI50v8TLJX6FL
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
