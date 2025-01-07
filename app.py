c2b8bb28913a7ac47f3f65104a2af733b6b2fa75
maaz khan <157114487+wtfmaaz@users.noreply.github.com>
2025-01-07 08:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def extract_skills(resume_text):
     tokens = resume_text.split()  # Tokenizing the resume text
 
     # Assuming the skills list from the CSV is already loaded
-    data = pd.read_csv(r"wtfmaaz/Resume-Classification/Cleaned_Resumes.csv")
+    data = pd.read_csv(r"Cleaned_Resumes.csv")
     skills = list(data.columns.values)  # Extract skill values from the CSV file
     skillset = []
 
